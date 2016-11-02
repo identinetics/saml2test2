1bb9d555066f37828afaf699286de2f76fd23cba
TWA <thomas@warwaris.at>
2016-11-02 16:12:57 +0100

    comhandler expansion

--- src/saml2test/comhandler.py
+++ src/saml2test/comhandler.py
@@ -2,31 +2,48 @@ import logging
 
 from aatest.comhandler import ComHandler as aatestComHandler
 from saml2.httputil import Redirect as SamlRedirect
+from saml2.httputil import Response as SamlResponse
 import urllib.request
 
 logger = logging.getLogger(__name__)
 
-class FakeRedirectResponse(object):
+class FakeAnResponse(object):
     def __init__(self,saml_response):
+        self.url = 'https://samlresponse.generated.by.local.test.driver'
+        self.content = ''
         self.saml_response = saml_response
+
+class FakeRedirectResponse(FakeAnResponse):
+
+    def __init__(self,saml_response):
+        super(FakeRedirectResponse,self).__init__(saml_response)
+
         if saml_response.status == '302 Found':
             redirect_url = saml_response.message
             self.status_code = 302
-            self.url = 'https://samlresponse.generated.by.local.test.driver'
-            self.content = ''
+            self.headers = {'location': redirect_url}
         else:
-            raise RuntimeError("SamlRedirect has an unknown status")
+            emsg = "SamlRedirect has an unknown status: {}".format(saml_response.status)
+            raise RuntimeError(emsg)
 
-        self.headers = {'location': redirect_url}
+class FakeResponseResponse(FakeAnResponse):
+    def __init__(self,saml_response):
+        super(FakeRedirectResponse,self).__init__(saml_response)
 
-
+        if saml_response.status == '200 OK':
+            self.status_code = 200
+        else:
+            emsg = "SamlRedirect has an unknown status: {}".format(saml_response.status)
+            raise RuntimeError(emsg)
 
 class ComHandler(aatestComHandler):
     def __call__(self, response, target_url='', auto_close_urls=None,
                  conv=None, **kwargs):
 
         if (isinstance(response, SamlRedirect)):
-          http_response = FakeRedirectResponse(response)
+            http_response = FakeRedirectResponse(response)
+        elsif (isinstance(response, SamlResponse)):
+            http_response = FakeResponseResponse(response)
         else:
             emsg = "ComHandler can not handle object of class {}".format(response.__class__.__name__)
             raise RuntimeError(emsg)
